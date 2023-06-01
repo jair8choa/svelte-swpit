b77d8452c576dd927f1e67baa338abcc3ef74ec6
jochoa <jair8choa@gmail.com>
2023-06-01 17:06:28 -0600

    edit vite.config

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,3 @@
-//const URLAPI = 'http://localhost:5050'
 const URLAPI = 'https://swpit-api-f7az4aatqq-uc.a.run.app'
 
 export {URLAPI}
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "https://jair8choa.github.io/swpit-svelte/",
   plugins: [svelte()],
 })
