c5ec42713834e9f418470ac13a3d8d3e5fd5cc40
Joaquin Jairo Ochoa Canela <43918449+jair8choa@users.noreply.github.com>
2023-06-01 20:48:12 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://jair8choa.github.io/svelte-swpit/",
+  base: "/svelte-swpit/",
   plugins: [svelte()],
   server:{
     host: '127.0.0.1'
